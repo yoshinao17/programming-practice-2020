3072a82fc6bb1a94485387b4da2900f420608ae7
｛Yoshinao Hara｝ <60565670+yoshinao17@users.noreply.github.com>
2020-03-02 16:00:31 +0900

    JS

--- Java Script/Udemy JS complete2020/script.js
+++ Java Script/Udemy JS complete2020/script.js
@@ -1,16 +1,26 @@
-var firstName= 'Yoshinao';
-console.log(firstName);
+// var firstName= 'Yoshinao';
+// console.log(firstName);
 
-var lastName = 'Hara'
-var age = 28;
+// var lastName = 'Hara'
+// var age = 28;
 
-var fullAge= false;
-console.log(fullAge);
+// var fullAge= false;
+// console.log(fullAge);
 
-job = 'Teacher';
-console.log(job);
+// job = 'Teacher';
+// console.log(job);
 
 var firstName ='John';
 var age = 28;
 
+//type coercion//
 console.log(firstName + ''+ age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '.Is he married?'  + isMarried  );
+
+//Variable mutation//
+
