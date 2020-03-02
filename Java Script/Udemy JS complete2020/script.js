6990d7f3621e0a21702670f74428d26a902958fc
｛Yoshinao Hara｝ <60565670+yoshinao17@users.noreply.github.com>
2020-03-03 00:44:34 +0900

    more js

--- Java Script/Udemy JS complete2020/script.js
+++ Java Script/Udemy JS complete2020/script.js
@@ -10,17 +10,67 @@
 // job = 'Teacher';
 // console.log(job);
 
-var firstName ='John';
-var age = 28;
+// var firstName ='John';
+// var age = 28;
 
-//type coercion//
-console.log(firstName + ''+ age);
+// //type coercion//
+// console.log(firstName + ''+ age);
 
-var job, isMarried;
-job = 'teacher';
-isMarried = false;
+// var job, isMarried;
+// job = 'teacher';
+// isMarried = false;
 
-console.log(firstName + ' is a ' + age + ' year old ' + job + '.Is he married?'  + isMarried  );
+// console.log(firstName + ' is a ' + age + ' year old ' + job + '.Is he married?'  + isMarried  );
 
 //Variable mutation//
+// age = ' twenty eight ';
+// job = ' driver ';
 
+// alert(firstName + ' is a ' + age + ' year old ' + job + ' Is he married? ' + isMarried ) ;
+
+// var lastName = prompt('What is his last name?');
+// console.log(firstName + '' + lastName);
+
+//Basic operators//
+
+// var year, yearJohn, yearMark;
+// now = 2018;
+// ageJohn= 28;
+// ageMark= 33;
+
+// yearJohn= now - ageJohn;
+// yearMark= now - ageMark;
+// console.log(yearJohn);
+
+
+// console.log(yearJohn);
+// console.log(now + 2); 
+// console.log(now * 2);
+// console.log(now / 10);
+
+// //Logical Operator
+// var johnOlder = ageJohn < ageMark;
+// console.log(johnOlder);
+
+// // typeof operator
+// console.log(typeof johnOlder);
+// console.log(typeof ageJohn)
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge =  18;
+
+//Multiple Operator
+var isFullAge = now - yearJohn >= fullAge;
+console.log(isFullAge);
+
+//Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) /2;
+console.log(average);
+
+//Multiple assignments
+var x, y;
+x = (3 + 5) * 4 -6; //8*4-6//32-6//26
+console.log(x);
