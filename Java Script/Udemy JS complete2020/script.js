458ad53d440deecf39e07d2c4a9eaf39d9d2caea
｛Yoshinao Hara｝ <60565670+yoshinao17@users.noreply.github.com>
2020-03-04 15:40:26 +0900

    added page2 with pics

--- Java Script/Udemy JS complete2020/script.js
+++ Java Script/Udemy JS complete2020/script.js
@@ -145,12 +145,15 @@ console.log('John\'s BMI is higher than Mark\'s.')
 
 //Boolean logic
 var firstName = 'John';
-var age=16;
+var age=20;
 
 if(age < 13){
   console.log(firstName + ' is a boy.');
 } else if (age >=13 && age < 20){
+  console.log(firstName + ' is a young man.');
+} else if(age >= 20 && age<30 ){
   console.log(firstName + ' is a teenager.');
+  
 
 }
 
