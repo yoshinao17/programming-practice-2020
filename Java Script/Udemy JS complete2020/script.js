6362f337efe4393a85041f55c31a92ec8dab9f7a
｛Yoshinao Hara｝ <60565670+yoshinao17@users.noreply.github.com>
2020-03-08 21:22:21 +0900

    js changed

--- Java Script/Udemy JS complete2020/script.js
+++ Java Script/Udemy JS complete2020/script.js
@@ -283,26 +283,63 @@ console.log('John\'s BMI is higher than Mark\'s.')
 //Functions
 
 
-function calculateAge(birthYear) {
-    return 2018 - birthYear;
+// function calculateAge(birthYear) {
+//     return 2018 - birthYear;
+// }
+
+// var ageJohn = calculateAge(1990);
+// var ageMike = calculateAge(1948);
+// var ageJane = calculateAge(1969);
+// console.log(ageJohn, ageMike, ageJane);
+
+// function yearsUntilRetirement(year, firstName){
+// var age= calculateAge(year);
+// var retirement = 65- age;
+
+// if(retirement > 0){
+// console.log(firstName + ' retires in '+ retirement + 'years');
+
+// }
+
+// else{
+// console.log(firstName + ' is already retired.')
+// }
+
+// }
+// yearsUntilRetirement (1985, 'Nao');
+
+// yearsUntilRetirement (1990, 'Mike');
+// yearsUntilRetirement (1940, 'Yoko');
+
+//function declaration
+// function whatDoYouDo(job, firstName){
+
+// }
+
+
+//function expression
+
+var whatDoYouDo = function(job, firstName){ switch(job) {
+    case 'teacher':
+      return firstName + ' teaches kids how to code';
+
+    case  'driver':
+      return firstName + ' drives a cab in Lisbon';
+    case 'designer':
+      return firstName + ' designs beautiful websites';
+
+      default:
+        return firstName + ' does somethingelse';
+                         }
 }
 
-var ageJohn = calculateAge(1990);
-var ageMike = calculateAge(1948);
-var ageJane = calculateAge(1969);
-console.log(ageJohn, ageMike, ageJane);
+console.log ( whatDoYouDo( 'teacher', 'John'));
 
-function yearUntilRetirement(year, firstName){
-var age= calculateAge(year);
+console.log(whatDoYouDo('driver', 'Yoko'));
 
+console.log(whatDoYouDo('designer', 'Nao'));
 
-}
-
-
-
-
-
-
+console.log(whatDoYouDo('futsal','Yuto'));
 
 
 
